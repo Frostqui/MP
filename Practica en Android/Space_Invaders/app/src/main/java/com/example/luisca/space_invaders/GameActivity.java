4da144dfaf5d7f8cd9a00c6df481d2e882e8a5f5
Frostqui <diego.lopezgarcia@hotmail.com>
2016-10-18 14:00:23 +0200

    Añadido sonidos

--- Practica en Android/Space_Invaders/app/src/main/java/com/example/luisca/space_invaders/GameActivity.java
+++ Practica en Android/Space_Invaders/app/src/main/java/com/example/luisca/space_invaders/GameActivity.java
@@ -2,6 +2,7 @@
 package com.example.luisca.space_invaders;
 
 
+import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -14,15 +15,20 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game); // Establece el xml con la vista
 
-    }
+        }
+    final MediaPlayer mp = MediaPlayer.create(this, R.raw.sound);
     Handler h2 = new Handler();
     Handler h3 = new Handler();
-    Runnable run2 = new Runnable() {
+
+
+      Runnable run2 = new Runnable() {
+
         @Override
         public void run() {
             ImageView bullet = (ImageView) findViewById(R.id.bullet);
@@ -31,26 +37,31 @@ public class GameActivity extends AppCompatActivity {
             getWindowManager().getDefaultDisplay().getMetrics(metrics);
             int width = metrics.widthPixels;
             int height = metrics.heightPixels;
-            bullet.setY(bullet.getY() - 60); //Mueve el disparo hacia arriba, el nº marca la velocidad del disparo.
-            if ((bullet.getY() + bullet.getHeight() < 10)) //Si el disparo sale de la pantalla desaparece
+
+
+            bullet.setY(bullet.getY()-30); //Mueve el disparo hacia arriba
+
+            if((bullet.getY()+bullet.getHeight() <10 )) //Si el disparo sale de la pantalla desaparece
             {
                 bullet.setVisibility(View.INVISIBLE);
                 h3.removeCallbacks(run2);
                 ship.setEnabled(true);
             }
+
             h3.postDelayed(this, 50);
         }
     };
-
-    public void shot(View v) { // Este metodo se ejecuta al pulsar sobre la nave
+    public void shot(View v){ // Este metodo se ejecuta al pulsar sobre la nave
         ImageView bullet = (ImageView) findViewById(R.id.bullet);
         ImageButton ship = (ImageButton) findViewById(R.id.ship);
-        bullet.setX(ship.getX() + ship.getWidth() / 2 - bullet.getWidth() / 2); //Ajustar centro de bala a centro de nave
-        bullet.setY(ship.getY() - ship.getHeight()); //Ajustar alto de la bala a final de la nave
+        bullet.setX(ship.getX()+ ship.getWidth()/2);
+        bullet.setY(ship.getY());
         bullet.setVisibility(View.VISIBLE);
         ship.setEnabled(false);
         h3.postDelayed(run2, 0);
+        mp.start();
     }
+
 
 
     @Override
@@ -58,8 +69,10 @@ public class GameActivity extends AppCompatActivity {
         float currX;
         ImageButton ship = (ImageButton) findViewById(R.id.ship);
         int action = MotionEventCompat.getActionMasked(event);
+
         switch (action) {
             case (MotionEvent.ACTION_DOWN):
+
                 DisplayMetrics metrics = new DisplayMetrics();
                 getWindowManager().getDefaultDisplay().getMetrics(metrics);
                 int width = metrics.widthPixels; // ancho absoluto en pixels
@@ -68,13 +81,19 @@ public class GameActivity extends AppCompatActivity {
                 Float x = event.getX();
                 ship.setX(x);
 
-            case (MotionEvent.ACTION_MOVE):
+            case MotionEvent.ACTION_MOVE:
                 currX = event.getRawX();
+
                 ship.setX(currX);
+
+
             default:
                 return super.onTouchEvent(event);
         }
     }
+
+
+
 
 
 }
