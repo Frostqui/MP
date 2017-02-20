9dac75980ba0424a5175def9cecf0311a640788f
pablogarciabenayas <pablogarciabenayas@gmail.com>
2017-02-20 14:03:52 +0100

    Nueva rama de prueba

---
+++ Código Final/Space Invaders/app/src/main/java/com/example/charl/spaceinvaders/Prueba.java
@@ -0,0 +1,14 @@
+package com.example.charl.spaceinvaders;
+
+/**
+ * Created by pablo on 20/02/17.
+ */
+
+public class Prueba {
+    private int x;
+    private int y;
+
+    private int metodoPrueba(){
+        return 1;
+    }
+}
